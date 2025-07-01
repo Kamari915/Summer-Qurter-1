c86bf217bc0cddd5e05aca34b638c84b9a255784
Kamari Revels <kamari.revels@hgs.hiddengeniusproject.org>
2025-07-01 15:35:36 -0700

    new

---
+++ Python/week-3/Debugging.py
@@ -0,0 +1,17 @@
+def PrintAge():
+    age = 30
+    print("Age:" + str(age))       
+    
+
+def Add(num1, num2): 
+    return num1 + num2
+
+def Sayhello ():
+    name = input("What is your name?")
+    print("Hello ", name)
+
+PrintAge()
+print(Add(5, 5))
+Sayhello()
+
+ 
