54ddcd7219471211b96eba2e4ca98435b45a8a68
Kamari Revels <kamari.revels@hgs.hiddengeniusproject.org>
2025-07-02 13:32:58 -0700

    new

--- Python/week-3/teamrandomizer.py
+++ Python/week-3/teamrandomizer.py
@@ -10,4 +10,18 @@ players = ["kamari" , "max" , "braylen"
 def PickTeams(players):
     random.shuffle(players)
     team1 = players[:len(players)//2]
-    teamCaptain1 = team1[random.randrange(0, 13)]
+    teamCaptain1= team1[random.randrange(0, 13)]
+
+    print("team1:")
+    print("teamCaptain1:" + teamCaptain1 )
+    for player in team1:
+        print(player)
+
+    team2 = players[:len(players)//2]
+    teamCaptain2= team2[random.randrange(0, 13)]
+    print("\n team2:")
+    print("teamCaptain2:" + teamCaptain2 )
+    for player in team2:
+        print(player)
+
+PickTeams(players)
